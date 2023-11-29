25e0cad06027bea5998a2f448c5d2a7ce4c03cc7
calvin3oo <calvinzboss@gmail.com>
2023-11-28 23:48:51 -0700

    commit

--- calc.erl
+++ calc.erl
@@ -1,5 +1,5 @@
 % src/calculator.erl
--module(calculator).
+-module(calc).
 -export([add/2, subtract/2, multiply/2, divide/2]).
 
 % Addition
@@ -11,6 +11,6 @@ subtract(X, Y) -> X - Y.
 % Multiplication
 multiply(X, Y) -> X * Y.
 
-% Division
-divide(X, Y) when Y /= 0 -> X / Y;
+% Division, rounded to two decimal places
+divide(X, Y) when Y /= 0 -> io:format("Result: ~p~n", [round((X * 100) / Y ) / 100 ]);
 divide(_, _) -> {error, "Division by zero"}.
